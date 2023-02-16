db3d7a08cfafe6dd11ede0fe6d37198962fd479f
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-02-16 14:18:22 -0800

    ggg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #define STDERR 2
 
+
 /**
 *main - prints exactly "and that place of art is useful"
 * - Dora Korgar, 2015-10-19",
 *followed by a new line, to the standard error.
+*
 *Return: Always 1(Not Success)
 */
 
