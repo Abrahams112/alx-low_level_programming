047dc22287efcfdf85122931e5542b0bbd88828b
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-08 10:08:28 -0800

    lop'

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - uses recursion
+ * @s: string in reverse
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_Putchar(s);
+	}
+}
