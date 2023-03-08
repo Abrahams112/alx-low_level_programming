30db7ed1cf54574e92c8e267a841f1db1fcc6577
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-08 11:09:02 -0800

    review

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial(int n) - print factoril of a given number
+ * @n: given number
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+
+	if (n <= 1 && >= 0)
+		return (n * factorial(n - 1));
+}
