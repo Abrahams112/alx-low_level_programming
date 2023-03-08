e8e617859f794481bb446d9709c7aedb5141589d
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-08 12:40:12 -0800

    king

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,13 @@
 /**
  * _print_rev_recursion - print reverse recuresion
  * @s: string in recure
+ * Return: void
  */
 void _print_rev_recursion(char *s);
 {
-	if (*s)
+	if (*s)/*if statement*/
 	{
-		_print_rev_recursion(s + 1);
-		_Putchar(*s);
+		_print_rev_recursion(s + 1);/*add s*/
+		_Putchar(*s);/*print s*/
 	}
 }
