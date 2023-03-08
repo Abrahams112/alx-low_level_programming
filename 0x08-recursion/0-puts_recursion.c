aabfb2e05eab306b47d21cab42f553293d29183c
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-08 15:32:54 -0800

    Oba nla

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * @s: string to rescue through
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
  * @s: string in recure
  * Return: void
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s)/*if statement*/
 	{
