3a0da5f5cbdd724e928c413409ab6f41dc158ae6
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-10 14:17:04 -0800

    waaaoh

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the result of the addition, followed by a new line
+ * @argc: argument counts
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
