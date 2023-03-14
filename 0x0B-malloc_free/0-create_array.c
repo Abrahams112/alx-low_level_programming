02f5f2e60ca152a8b4cc76415b9adf28dd9a67c8
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-14 07:31:51 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,11 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-
-	t = malloc(sizeof(char) * size)
+:x
 
 	if (t == NULL)
 		return (NULL);
-	for (i = 0; i < siz; i++)
+	for (i = 0; i < size; i++)
 		t[1] = c;
 	return (t);
 }
