a8be6717728d1e5b800061f2b784225e8d1d8f7e
Gideon Jesuyon <abrahamgideon11@gmail.com>
2023-03-20 16:14:32 -0700

    tases

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,3 @@
+#ifndef SIZE
+#define SIZE 1034
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - program that prints ne of a file 
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
