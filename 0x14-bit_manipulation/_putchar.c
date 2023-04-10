b2e1c5a60ba5e82cb60e1558cda7615b005999a4
Abrahams112 <Abrahamgideon11@gmail.com>
2023-04-10 14:28:59 -0700

    task 1

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the  character c to stdout
+ * @c: The character to print 
+ *
+ * Return: On success 1.
+ * On erroe. -1 is returned, and error js set appropraitely.
+ */
+int _putchat(char c)
+{
+	return (write(1, &c, 1));
+}
