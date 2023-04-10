9da299589cff65ae68190de63cd3586d51c5b411
Abrahams112 <Abrahamgideon11@gmail.com>
2023-04-10 14:47:25 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,25 @@
 #include "main.h"
 /**
- * binary_to_uint - binary to unit
+ * binary_to_uint - binary to uint
  * @b: pointing to a string of 0 and 1 chars
- * Return: unsighed int
+ * Return: unsigned int
  *
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int Return = 0;
+
 	if (b == NULL)
-	{
 		return (0);
-	}
-	unsigned int result = 0;
-	int i = 0;
-
-	while (b[i] != '\0')
-
+	while (*b)
 	{
-		if (b[i] == '0')
-		{
-			result = result << 1;
-		}
-		else if (b[i] == '1')
-		{
-			result = (result << 1) | 1;
-		}
+		if (*b == '1')
+			Return = (Return << 1) | 1;
+		else if (*b == '0')
+			Return  <<= 1;
 		else
-		{
 			return (0);
-		}
-		i++;
+		b++;
 	}
-	return (result);
+	return (Return);
 }
-
