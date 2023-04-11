532ba9d4a48dd88eb5f3163738843817057ee39a
Abrahams112 <Abrahamgideon11@gmail.com>
2023-04-10 19:30:53 -0700

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,6 +6,7 @@
  * @argv: The pointer to array of arguments
  * Return: 1 on success, error code on failure
  */
+
 int main(int argc, char *argv[])
 {
 	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
